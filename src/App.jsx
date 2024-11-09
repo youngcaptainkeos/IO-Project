bdb571f2a847118349d0c51a6cc097b4fb46f849
Shashank Keshava Murthy <67853396+youngcaptainkeos@users.noreply.github.com>
2024-11-09 22:21:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Introduction from './pages/Introduction';
 import MemeGenerators from './pages/MemeGenerator';
-import FunFacts from './pages/FunFacts';
 import About from './pages/About';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -25,7 +24,6 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/fandom" element={<Fandom />} />
-          <Route path="/FunFacts" element={<FunFacts />} />
           <Route path="/DeepShowerThoughts" element={<DeepShowerThoughts />} />
           <Route path="/Phobia" element={<Phobia />} />
         </Routes>
